239281ad4cc6884ee58751526d533abb92d34617
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2024-04-06 14:08:57 -0500

    imports, structure, style
    linked the import page, and finalized a starting point for the html
    
    css has been started for grid areas and basic layout.
    
    javascript to put info from databases into page

---
+++ import-this.js
@@ -0,0 +1,73 @@
+const projects = [
+  {
+    name: "Project Name 1",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vel excepturi asperiores veritatis nam autem minus nisi rem? Labore exercitationem tempora quisquam magni eum maxime tenetur et voluptate est placeat.",
+    icons: [`<i class="fa-solid fa-star"></i>`, `<i class="fa-solid fa-comment-dots"></i>`, `<i class="fa-solid fa-poo"></i>`],
+  },
+  {
+    name: "Project Name 2",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vel excepturi asperiores veritatis nam autem minus nisi rem? Labore exercitationem tempora quisquam magni eum maxime tenetur et voluptate est placeat.",
+    icons: [`<i class="fa-solid fa-star"></i>`, `<i class="fa-solid fa-comment-dots"></i>`, `<i class="fa-solid fa-poo"></i>`],
+  },
+  {
+    name: "Project Name 3",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vel excepturi asperiores veritatis nam autem minus nisi rem? Labore exercitationem tempora quisquam magni eum maxime tenetur et voluptate est placeat.",
+    icons: [`<i class="fa-solid fa-star"></i>`, `<i class="fa-solid fa-comment-dots"></i>`, `<i class="fa-solid fa-poo"></i>`],
+  },
+  {
+    name: "Project Name 4",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vel excepturi asperiores veritatis nam autem minus nisi rem? Labore exercitationem tempora quisquam magni eum maxime tenetur et voluptate est placeat.",
+    icons: [`<i class="fa-solid fa-star"></i>`, `<i class="fa-solid fa-comment-dots"></i>`, `<i class="fa-solid fa-poo"></i>`],
+  },
+  {
+    name: "Project Name 5",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vel excepturi asperiores veritatis nam autem minus nisi rem? Labore exercitationem tempora quisquam magni eum maxime tenetur et voluptate est placeat.",
+    icons: [`<i class="fa-solid fa-star"></i>`, `<i class="fa-solid fa-comment-dots"></i>`, `<i class="fa-solid fa-poo"></i>`],
+  },
+  {
+    name: "Project Name 6",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vel excepturi asperiores veritatis nam autem minus nisi rem? Labore exercitationem tempora quisquam magni eum maxime tenetur et voluptate est placeat.",
+    icons: [`<i class="fa-solid fa-star"></i>`, `<i class="fa-solid fa-comment-dots"></i>`, `<i class="fa-solid fa-poo"></i>`],
+  },
+];
+
+const announcements = [
+  {
+    name: "Announcement Name 1",
+    description: "this is an announcement. you should read it. you could start to prepare for it.",
+  },
+  {
+    name: "Announcement Name 2",
+    description: "this is an announcement. you should read it. you could start to prepare for it.",
+  },
+  {
+    name: "Announcement Name 3",
+    description: "this is an announcement. you should read it. you could start to prepare for it.",
+  },
+];
+
+const trendings = [
+  {
+    pfp: "https://robohash.org/asdfadsfas",
+    name: "@pressdat",
+    description: "button presser",
+  },
+  {
+    pfp: "https://robohash.org/tqwerwegh",
+    name: "@logmeso",
+    description: "console logger",
+  },
+  {
+    pfp: "https://robohash.org/hrterhs",
+    name: "@sorequest",
+    description: "fetch requester",
+  },
+  {
+    pfp: "https://robohash.org/dsfafaf",
+    name: "@somuchwait",
+    description: "waiter",
+  },
+  
+];
+
+export { projects, announcements, trendings };
---
+++ script.js
@@ -0,0 +1,40 @@
+import { projects, announcements, trendings } from "./import-this.js";
+
+const projectCards = document.querySelector(".projects");
+const announcementCards = document.querySelector(".announcements");
+const trendingCards = document.querySelector(".trending");
+
+projects.forEach((project) => {
+  const card = document.createElement("div");
+  card.classList.add("project-card");
+  card.innerHTML = `<h3>${project.name}</h3>
+  <p>${project.description}</p>
+  <div class="icons">
+    ${project.icons
+      .map((icon) => {
+        return icon;
+      })
+      .join("")}
+  </div>`;
+  projectCards.appendChild(card);
+});
+
+announcements.forEach((announcement) => {
+  const card = document.createElement("div");
+  card.classList.add("announcement-card");
+  card.innerHTML = `<h3>${announcement.name}</h3>
+  <p>${announcement.description}</p>`;
+  announcementCards.appendChild(card);
+});
+
+trendings.forEach((trend) => {
+  const card = document.createElement("div");
+  card.classList.add("trending-card");
+  card.innerHTML = `<img src="${trend.pfp}"/>
+  <div>
+    <h3>${trend.name}</h3>
+    <p>${trend.description}</p>
+  </div>`;
+  trendingCards.appendChild(card);
+})
+
